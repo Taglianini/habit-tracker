b6414bcfbb3c585a1d097c9b16bcf790cc89b226
Taglianini <tomastagliani1234@gmail.com>
2025-01-08 22:54:37 -0300

    Funcion Agregar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { AddHabit } from './components/AddHabit'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/AddHabit";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AddHabit />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
