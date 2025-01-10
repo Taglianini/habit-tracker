f2fc1c606ec54deb500e92acd2fdcbd0f1bbc4ab
MitsuyaT <lautyrodriguez25@gmail.com>
2025-01-09 21:45:12 -0300

    Agregue checkbox y semantica - AddHabit

--- src/components/AddHabit.jsx
+++ src/components/AddHabit.jsx
@@ -3,19 +3,22 @@ import React, { useState } from "react";
 const App = () => {
   const [contador, setContador] = useState(0);
 
-  const AgregarDiv = () => setContador(contadorAntes => contadorAntes + 1);
+  const sumaLongitud = () => setContador(contador => contador + 1);
 
   return (
     <div>
       <h1>Habitos</h1>
-      <button className="boton-div" onClick={AgregarDiv}>
+      <button className="boton-div" onClick={sumaLongitud}>
         Agregar
       </button>
 
       <div style={{ marginTop: "20px" }}>
         {Array.from({ length: contador }, (_, index) => (
           <div key={index} className="generated-div">
-            {index + 1}
+            <label>
+              <input type="checkbox" />
+               Hábito {index + 1}
+            </label>
           </div>
         ))}
       </div>
